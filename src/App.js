f247daf80ac8699559b4e96bdba04dccdb21cd0e
Chris Anderson <c.anderson2@ymail.com>
2024-08-31 13:28:21 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import avatar from './images/avatar-jessica.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="avatar-container">
+        <img src={avatar} alt={"Avatar"} className="avatar"></img>
+      </div>
+      
+      <div className="name u-push-top-24 text-preset-1">Jessica Randall</div>
+      <div className="location u-push-bottom-24 text-preset-2__bold">London, United Kingdom</div>
+      <div className="description u-push-bottom-24 text-preset-2">"Front end developer and avid reader."</div>
+
+      <div className="button-container text-preset-2__bold">
+        <button className="button">Github</button>
+        <button className="button">Frontend Mentor</button>
+        <button className="button">LinkedIn</button>
+        <button className="button">Twitter</button>
+        <button className="button">Instagram</button>
+      </div>
     </div>
   );
 }
